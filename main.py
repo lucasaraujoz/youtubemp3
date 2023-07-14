8123ce732b7bb5f5a1f6bd74816c620d3ee11972
arthurgsf <arthurgsf@nca.ufma.br>
2023-07-14 19:14:55 -0300

    adicionada conversão com ffmpeg, remoção de arquivos após download e outros refactors

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 import yt_dlp
+import ffmpeg
+import pathlib
+import os
 
 app = Flask(__name__)
 
@@ -7,25 +10,47 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         url = request.form['url']
+        format = request.form['format']
         try:
-            URLS = [url]
-            ydl_opts = {
-                'format': 'm4a/bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'm4a',
-                }],
-                'o': '/output/%(title)s.%(ext)s'  # Define o padrão de nomeação e o diretório de destino
-
-            }
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                info = ydl.extract_info(url, download=True)
-                filename = ydl.prepare_filename(info)
-                return send_file(filename, as_attachment=True)
+            filename = download_video(url, format)
+            fp = open(filename, "rb")
+            def stream():
+                yield from fp
+                fp.close()
+                os.remove(filename)
+            return app.response_class(
+                stream(), 
+                headers={'Content-Disposition': f'attachment; filename={pathlib.Path(filename).name}'},
+                mimetype=f"audio/{format}"
+            )
         except Exception as e:
-            message = f'Ocorreu um erro ao processar o vídeo. Detalhes: {str(e)}'
-            return render_template('index.html', message=message)
+            return render_template('index.html', message=str(e))
+
     return render_template('index.html')
+
+def download_video(url, format):
+    if format not in ["3gp","aac","flv","m4a","mp3","mp4","ogg","wav","webm"]:
+        raise "Formato indisponível, escolha uma das opções: [3gp, aac, flv, m4a, mp3, mp4, ogg, wav, webm]."
+    
+    ydl_opts = {
+        'format': "bestaudio",
+        'o': '/output/%(title)s.%(ext)s'  # Define o padrão de nomeação e o diretório de destino
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
+
+    ext = pathlib.Path(filename).suffix
+    if format != ext:
+        new_filename = filename.replace(ext, "."+format)
+        downloaded = ffmpeg.input(filename)
+        converted = ffmpeg.output(downloaded, new_filename)
+        ffmpeg.run(converted, overwrite_output=True)
+        os.remove(filename)
+        return new_filename
+    
+    return filename
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
